c249f27d442f1d2e7cf068c4ddc57af4554090c9
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-26 15:22:45 -0300

    aula 01- escopo

---
+++ 01-escopo-no-javascript/index.js
@@ -0,0 +1,38 @@
+let numero = 123
+
+function falar() {
+  const mensagem = 'Olá Mundo'
+  console.log(mensagem)
+  function falarOutraCoisa() {
+
+  }
+
+  falarOutraCoisa()
+  console.log(numero)
+
+}
+  
+falar()
+console.log(numero)
+
+// if(true) {
+//   const mensagem = 'Olá'
+//   console.log(mensagem)
+// }
+
+// {
+//   var mensagem = 'Olá'
+//   console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+// function falar() {
+//   const mensagem = 'Olá Mundo'
+//   console.log(mensagem)
+// }
+
+// falar()
+// console.log(mensagem)
+
+
