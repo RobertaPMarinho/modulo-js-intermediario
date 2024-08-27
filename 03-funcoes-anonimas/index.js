46c80c75b9fb277f31c7e3309d2fd29ae4687eef
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-26 22:13:02 -0300

    aula 03-funções anônimas

---
+++ 03-funcoes-anonimas/index.js
@@ -0,0 +1,14 @@
+//VERSÃO ANTIGA
+
+// function somar() {
+//   return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+// console.log(resultadoDaSoma)
+
+let resultadoDaSoma = function(numero1, numero2){
+  return numero1 + numero2
+}
+
+console.log(resultadoDaSoma)
