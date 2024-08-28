813779590b9bfb3d72c41f31f2cde048a356a0b3
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-28 15:30:16 -0300

    aula 07-arrays multidimensional

---
+++ 07-arrays-mutidimensional/index.js
@@ -0,0 +1,12 @@
+let filmes = [
+  ["Vingadores", 4.5, true],
+  ["Mulher Maravilha", 4.6, false, ['GalGadot', 25]]
+];
+
+console.table(filmes[1][3][0])
+
+
+
+
+
+//descricaoFilme = ['Vingadores', 4.5, true]
