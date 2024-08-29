651b3a60265572f20ded2dfe51be63ecb65dedc7
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-29 15:53:11 -0300

    aula 09-slice e splice

---
+++ 09-splice-e-slice/index.js
@@ -0,0 +1,21 @@
+let frutas = ['banana', 'maçã', 'laranja', 'uva']
+
+
+//SLICE
+// let frutasExtraidas = frutas.slice(1)
+
+// console.log(frutasExtraidas)
+
+// console.log(frutas)
+
+//SPLICE
+
+// let frutasExtraidas = frutas.splice(1, 2)
+
+// console.log(frutasExtraidas)
+
+// console.log(frutas)
+
+let removeEAdicionaFrutas = frutas.splice(1, 0, 'goiaba', 'abacaxi', 'pera')
+
+console.log(frutas)
