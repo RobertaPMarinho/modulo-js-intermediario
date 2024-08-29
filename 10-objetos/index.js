13f50304a75d7a8b367f2af92be914c4839431f8
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-29 16:25:21 -0300

    aula 10-objetos

---
+++ 10-objetos/index.js
@@ -0,0 +1,14 @@
+let tenis = {
+  //chave: valor
+  cor: 'branco',
+  tamanho: 42,
+  emEstoque: true
+}
+
+//console.log(tenis.cor)
+
+//console.log(tenis['tamanho'])
+
+tenis.valor = 200
+
+console.log(tenis)
