07aa79457f7d3cecb323e2c471b178348e2ea7f8
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-28 21:20:26 -0300

    aula 08-trabalhando com arrays

---
+++ 08-trabalhando-com-arrays/index.js
@@ -0,0 +1,18 @@
+//let frutas = ['banana', 'maçã', 'laranja', 'pêra']
+//let frutasTamanho = frutas.length
+
+//console.log(frutasTamanho)
+
+// for(let i = 0; i < frutasTamanho; i++){
+//   console.log('Fruta: ' + frutas[i])
+// }
+
+// frutas.forEach(function(item, indice, array){
+//   console.log('Fruta: ' + item)
+// })
+
+let frutas = ['banana', 'maçã', 'laranja', 'pêra']
+
+let posicaoLaranja = frutas.indexOf('laranja')
+
+console.log(posicaoLaranja)
