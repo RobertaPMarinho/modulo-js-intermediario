b8bf84376c32145f1e3e8d4874ca2d5a2f369943
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-09-09 23:05:22 -0300

    aula 18-manipulando atributos e estilos dos elementos do DOM

--- 18-manipulando-atributos-elementos-dom/index.js
+++ 18-manipulando-atributos-elementos-dom/index.js
@@ -1,12 +1,19 @@
 function alterarCorFundoPrimeiroPost() {
-  let primeiroPost = document.getElementsByClassName("post")[0];
-  primeiroPost.style.backgroundColor = "lightPink";
+  let posts = document.getElementsByClassName('post')
+
+  console.log(posts)
+
+  let primeiroPost = posts[0]
+  console.log('primeiroPost:', primeiroPost)
+  primeiroPost.style.backgroundColor = 'red'
 }
 
 function aumentarFonteSegundoPost() {
-  let segundoPost = document.getElementsByClassName("texto-post")[1];
+  let textoPosts = document.getElementsByClassName("texto-post");
 
-  segundoPost.classList.add("fonte-grande"); // Para acessar a lista de classes de determinado elemento utilizamos o método classList. Para adicionar elementos à essa lista, utilizamos o método add
+  console.log(textoPosts[1].classList)
+
+  textoPosts[1].classList.add('fonte-grande'); // Para acessar a lista de classes de determinado elemento utilizamos o método classList. Para adicionar elementos à essa lista, utilizamos o método add
 }
 
 // 1) Criar uma função com instruções do que deve acontecer a depender se clicamos no botão X ou se clicamos no botão Y (condicional if/else). Para isso também é necessário adicionar um parâmetro à ela. Para que ela dispare é necessário que os elementos button tenham o atributo onclick com o valor sendo essa função.
@@ -17,9 +24,9 @@ function marcarRadio(genero) {
   let generoFeminino = document.getElementById("genero-feminino");
 
   // 3) Criar uma condicional para definir que, se o parâmetro passado na função for igual a “M”, o atributo checked deve ser atribuído ao botão “Marcar radio masculino” através do método também chamado checked. Mas, se o parâmetro passado na função for igual a “F”, o atributo checked deve ser atribuído ao botão “Marcar radio feminino”. Os parâmetros “M” e “F” já foram passados na criação dos botões no documento HTML.
-  if (genero === "M") {
+  if (genero === 'M') {
     generoMasculino.checked = true;
-  } else if (genero === "F") {
+  } else if (genero === 'F') {
     generoFeminino.checked = true;
   }
 }
