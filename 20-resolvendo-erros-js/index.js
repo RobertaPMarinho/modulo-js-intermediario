228934458cb83f245ec8356debd08f792401b95c
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-09-11 16:15:01 -0300

    aula 20-encontrando e interpretando erros no JS

---
+++ 20-resolvendo-erros-js/index.js
@@ -0,0 +1,13 @@
+// let valorBoleto = 100
+
+// console.log(valorBoleto)
+
+// console.log(valorBolet)
+
+let inputTelefone = document.getElementById('telefone')
+
+inputTelefone.disabled = true
+
+function habilitarTelefone() {
+  
+}
